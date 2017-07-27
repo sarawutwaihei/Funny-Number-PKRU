e6962900c922855d15e17c180b671ff90c0d1c85
Com03 <com03@Com03s-iMac.local>
2017-07-27 16:39:17 +0700

    Create Class

---
+++ FunnyNumber/MyClass.swift
@@ -0,0 +1,16 @@
+//
+//  MyClass.swift
+//  FunnyNumber
+//
+//  Created by Com03 on 7/27/2560 BE.
+//  Copyright © 2560 Com03. All rights reserved.
+//
+
+import Foundation
+
+class MyClass {
+
+    let strName = "App"
+    
+    
+} // My Class
--- FunnyNumber/ViewController.swift
+++ FunnyNumber/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
+    @IBAction func decButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func incButton(_ sender: Any) {
+    }
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let testClass = MyClass()
+        print("Name ==> \(testClass.strName)")
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
